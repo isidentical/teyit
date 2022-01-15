17c95a59803bc96ecdb8ff4f3126c038e032671d
Batuhan Taskaya <isidentical@gmail.com>
2022-01-15 16:05:42 +0300

    Preserve formatting on indented literals

--- tests/cosmetic/literals/input.py
+++ tests/cosmetic/literals/input.py
@@ -15,3 +15,17 @@ class TestCase(unittest.TestCase):
             2                     + 2,
             False
         )
+
+    def test_indented_long_string(self):
+        self.assertTrue(
+            "Rejected:\n"
+            "Launchpad failed to process the upload path '~name16/ubuntu':\n\n"
+            "unicode PPA name: áří is disabled.\n\n"
+            "It is likely that you have a configuration problem with "
+            "dput/dupload.\n"
+            "Please check the documentation at "
+            "https://help.launchpad.net/Packaging/PPA/Uploading and update "
+            "your configuration.\n\n"
+            "Further error processing not possible because of a critical "
+            "previous error." in body
+        )
--- tests/cosmetic/literals/output.py
+++ tests/cosmetic/literals/output.py
@@ -13,3 +13,18 @@ class TestCase(unittest.TestCase):
         self.assertFalse(
             2                     + 2
         )
+
+    def test_indented_long_string(self):
+        self.assertIn(
+            "Rejected:\n"
+            "Launchpad failed to process the upload path '~name16/ubuntu':\n\n"
+            "unicode PPA name: áří is disabled.\n\n"
+            "It is likely that you have a configuration problem with "
+            "dput/dupload.\n"
+            "Please check the documentation at "
+            "https://help.launchpad.net/Packaging/PPA/Uploading and update "
+            "your configuration.\n\n"
+            "Further error processing not possible because of a critical "
+            "previous error.",
+            body
+        )
